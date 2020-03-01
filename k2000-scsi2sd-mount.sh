2bfff0af764a31e5fb9b2448aff63540410f25e8
Tony <bonafide@martica.org>
2020-03-01 13:49:00 -0800

    Add files via upload

--- k2000-scsi2sd-mount.sh
+++ k2000-scsi2sd-mount.sh
@@ -11,8 +11,8 @@ else
  sdcard=$1
 fi
 mkdir -p /media/{scsi0,scsi1,scsi2,scsi3}
-mount -t msdos -o loop,offset=0,sizelimit=2147483136 $sdcard /media/scsi0/
-mount -t msdos -o loop,offset=2147483136,sizelimit=2147483136 $sdcard /media/scsi1/
-mount -t msdos -o loop,offset=4294966272,sizelimit=2147483136 $sdcard /media/scsi2/
-mount -t msdos -o loop,offset=6442449408,sizelimit=2147483136 $sdcard /media/scsi3/
+mount -v -t msdos -o loop,offset=0,sizelimit=2147483136 $sdcard /media/scsi0/
+mount -v -t msdos -o loop,offset=2147483136,sizelimit=2147483136 $sdcard /media/scsi1/
+mount -v -t msdos -o loop,offset=4294966272,sizelimit=2147483136 $sdcard /media/scsi2/
+mount -v -t msdos -o loop,offset=6442449408,sizelimit=2147483136 $sdcard /media/scsi3/
 exit 0
