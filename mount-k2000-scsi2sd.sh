26201425adc764804176e31ddbb4f9300cab3625
Tony <bonafide@martica.org>
2020-03-01 13:39:05 -0800

    Add files via upload

--- mount-k2000-scsi2sd.sh
+++ mount-k2000-scsi2sd.sh
@@ -4,10 +4,15 @@ if [ "$EUID" -ne 0 ]; then
  echo "Please run this script as root!"
  exit 1
 fi
-sdcard=/dev/sdc
-mkdir -p {scsi0,scsi1,scsi2,scsi3}
-mount -t msdos -o loop,offset=0,sizelimit=2147483136 $sdcard scsi0/
-mount -t msdos -o loop,offset=2147483136,sizelimit=2147483136 $sdcard scsi1/
-mount -t msdos -o loop,offset=4294966272,sizelimit=2147483136 $sdcard scsi2/
-mount -t msdos -o loop,offset=6442449408,sizelimit=2147483136 $sdcard scsi3/
+if [ -z "$1" ]; then
+ echo -n "Please enter the path to your sdcard (ie. /dev/sdc): "
+ read sdcard
+else
+ sdcard=$1
+fi
+mkdir -p /media/{scsi0,scsi1,scsi2,scsi3}
+mount -t msdos -o loop,offset=0,sizelimit=2147483136 $sdcard /media/scsi0/
+mount -t msdos -o loop,offset=2147483136,sizelimit=2147483136 $sdcard /media/scsi1/
+mount -t msdos -o loop,offset=4294966272,sizelimit=2147483136 $sdcard /media/scsi2/
+mount -t msdos -o loop,offset=6442449408,sizelimit=2147483136 $sdcard /media/scsi3/
 exit 0
